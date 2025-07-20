90f0974c44de6d33d061d0d2b6e932892583ac6f
나제법 <skwpqjq@gmail.com>
2025-07-20 15:44:24 +0900

    test: 샘플 테스트 추가

--- src/test/java/com/example/demo/SampleTest.java
+++ src/test/java/com/example/demo/SampleTest.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +8,7 @@ public class SampleTest {
     @Test
     @DisplayName("샘플 테스트를 진행한다.")
     void exampleTest() {
-        Assertions.fail();
+
     }
 
 }
